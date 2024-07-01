b5a55cfd079e406cf811b13d585c78adbf6d5443
Vlad <vladmakarenko@Vlads-MacBook-Pro.local>
2024-07-01 13:15:44 +0200

    init

---
+++ HealthcareApp/src/LearnMultithreading/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace LearnMultithreading;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var obj = new object();
+        List<string> names = new List<string>();
+        var thread1 = new Thread(() =>
+        {
+            lock (obj)
+            {
+                names.Add("Denis");
+            }
+        });
+        var thread2 = new Thread(() =>
+        {
+            lock (obj)
+            {
+                names.Add("Kolya");
+            }
+        });
+        
+        
+        thread1.Start();
+        thread2.Start();
+    }
+}
